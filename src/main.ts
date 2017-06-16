fddea1c0466291f554d0c48a1f76530a4731c0b0
Ganguly, Biswajit (US - Hyderabad) <bganguly@deloitte.com>
2017-06-16 18:16:19 +0530

    @Initiate project

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import 'core-js';
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
