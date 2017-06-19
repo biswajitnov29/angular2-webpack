8a430a80ecf6b7f2cab5963a6024d8d4e3622fc8
Ganguly, Biswajit (US - Hyderabad) <bganguly@deloitte.com>
2017-06-19 16:54:11 +0530

    Modify Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+import '../assets/css/styles.css';
+
 @Component({
-    selector:'my-app',
-    template:'<h1>My App</h1>'
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    
-}
+export class AppComponent { }
